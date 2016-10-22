8977d02afd581fca77ba9b974ce78c1f3c04023e
jrhemmin <jrhemmin@asu.edu>
2016-10-22 12:36:00 -0700

    Update BowAmmo.java

--- BowAmmo.java
+++ BowAmmo.java
@@ -13,8 +13,8 @@ public class BowAmmo extends Bullets {
 //Sets attributes for the arrow for the crossbow
 //------------------------------------------------------------------------------    
     private final String ammoType = "Arrow";
-    private final int ammoSpeed = 8; 
-    private final int damage = 8;
+    private final int ammoSpeed = 8; //plan on changing this, just a filler for now
+    private final int damage = 4;
 
     public String SetAmmoType(){
          return ammoType;
